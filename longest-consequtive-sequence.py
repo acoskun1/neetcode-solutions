e91cbb8d17a2e72799db4825d8c57125fef0582f
Ali Coskun <acoskunbiz@gmail.com>
2024-10-21 01:01:24 +0100

    add bruteforce and sorting solutions

--- longest-consequtive-sequence.py
+++ longest-consequtive-sequence.py
@@ -25,52 +25,68 @@ class Solution:
             After a num is processed in a while loop, it is marked true and not processed again in subsequent iterations. Each element is processed constant number of times regardless of array size. 
         """
 
-
-        longest_sequence = 0
-
-        #create a hashmap that stores each element of nums as key and false as their value
-        hmap = {num: False for num in self.nums}
-        
+        ans = 0
+        hashNums = {num : False for num in self.nums}
         for num in self.nums:
+            curr = 1
+            nextel = num + 1
+            while nextel in hashNums and not hashNums[nextel]:
+                curr += 1
+                hashNums[nextel] = True
+                nextel +=1
 
-        #create forward check
-            #for each num in iteration set current_sequence to 1
-            #if there is no next or previous element sequence is 1
-            current_sequence = 1
+            prev = num - 1
+            while prev in hashNums and not hashNums[prev]:
+                curr += 1
+                hashNums[prev] = True
+                prev -= 1
 
-            #next element in the sequence will be num + 1
-            next_element = num + 1
-            
-            #if next_element is in hmap and false, increment current_sequence and set hmap[next_element] True.
-            #repeat until next_element not in the hmap or True
-            while not hmap[next_element] and next_element in hmap:
-                current_sequence += 1
-                hmap[next_element] = True
-                next_element += 1
-
-        #create backward check
-            #previous element in the sequence is num - 1
-            prev_element = num - 1
-            
-            #if previous element in hmap and false, increment current_sequence and set hmap[next_element] True
-            #repeat until previous element not in the hmap or False.
-            while prev_element in hmap and not prev_element:
-                current_sequence += 1
-                hmap[prev_element] = True
-                prev_element -= 1
-
-        #set longest_sequence to the maximum of longest_sequence and current_sequence
-        #this will set longest_sequence to current_sequence only if current_sequence is greater than longest_seq.
-        
-            longest_sequence = max(longest_sequence, current_sequence)
-
-        return longest_sequence
+            ans = max(ans, curr)
+        return ans
 
     def longestConsecutiveBruteForce(self) -> int:
         """
         Time Complexity & Space Complexity: O(N^2) 
         """
-        pass
+        
+        if not self.nums: 
+            return 0
+
+        ans = 0
+        for num in self.nums:
+            curr_num = num
+            curr_streak = 1
+
+            while curr_num + 1 in self.nums:
+                curr_num += 1
+                curr_streak += 1
+
+            ans = max(ans, curr_streak)
+
+        return ans
+
+    def longesConsecutiveSorting(self) -> int:
+        """
+        Time Complexity: O(n log N)
+        Space Complexity: O(1)
+        """
+        
+        if not self.nums:
+            return 0
+
+        self.nums.sort()
+        ans = 1
+        curr_streak = 1
+
+        for i in range(1, len(self.nums)):
+            if self.nums[i] == self.nums[i-1]:
+                continue
+            if self.nums[i] == self.nums[i-1] + 1:
+                curr_streak += 1
+            ans = max(ans, curr_streak)
+            curr_streak +=1
+
+        return max(ans, curr_streak)
 
 
 if __name__ == "__main__":
