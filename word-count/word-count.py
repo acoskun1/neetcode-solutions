f87f193cde4c6649851f4a9681a59deceeb48b5a
acoskun1 <acoskunbiz@gmail.com>
2025-06-12 02:09:19 +0100

    word-count edits

--- word-count/word-count.py
+++ word-count/word-count.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python3
+
 """
 Given a text file count most repeated words, list them, sort, and show number of repeatings.
 """
+import sys
+from collections import defaultdict
+
+def word_count(file: str) -> dict:
+    word_map = defaultdict(int)
+    with open(file, 'r') as text:
+
+    pass
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: --')
+        sys.exit(1)
+    ans = word_count(sys.argv[1])
