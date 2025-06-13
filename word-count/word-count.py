a990c55bb61dab0c9e5850bd4dff62035b8f5bb6
acoskun1 <acoskunbiz@gmail.com>
2025-06-13 01:01:55 +0100

    complete word-count

--- word-count/word-count.py
+++ word-count/word-count.py
@@ -2,15 +2,47 @@
 
 """
 Given a text file count most repeated words, list them, sort, and show number of repeatings.
+
+Approach:
+
+    strip and split text
+    chunk read without loading entire text file to memory
+    count words into hashmap
+    bucket sort
 """
 import sys
 from collections import defaultdict
 
-def word_count(file: str) -> dict:
+def word_count(file: str, chunk_size: int, k: int = 0) -> dict:
     word_map = defaultdict(int)
-    with open(file, 'r') as text:
 
-    pass
+    with open(file, 'r') as text:
+        while True:
+            chunk = text.read(chunk_size)
+            if not chunk:
+                break
+
+            for word in chunk.lower().split():
+                if word:
+                    word_map[word] = word_map.get(word, 0) + 1
+
+    max_count = max(word_map.values(), 0)
+    freq = [[] for _ in range(max_count + 1)]
+    for word, count in word_map.items():
+        freq[count].append(word)
+
+    """
+    if asked top k frequent:
+
+    ans = []
+    for i in range(len(nums), 0, -1):
+        for value in freq[i]:
+            ans.append(value)
+            if len(ans) == k:
+                return ans
+    """
+
+    return freq[-1].sort()
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
