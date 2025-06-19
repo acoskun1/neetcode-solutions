f12b14856e56d4c69e32896ae114dd0b1dd777ba
acoskun1 <acoskunbiz@gmail.com>
2025-06-19 01:09:29 +0100

    plan directory_size

--- directory-size/directory-size.py
+++ directory-size/directory-size.py
@@ -4,3 +4,6 @@ given each item is either folder with id of the items inside of it,
 or a file with name and size.
 """
 
+#If can be represented as a graph, using DFS you can identify (in the stack | queue) if your item is a 
+#directory, if a directory continue DFS and increment a total count if you find a valid file node.
+
